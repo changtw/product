917037cd657acf80990adaf99d28fe06816b13b1
changtw <yhocliff@gmail.com>
2018-05-24 21:42:35 +0800

    third

--- product.py
+++ product.py
@@ -1,11 +1,25 @@
-proudct = []
+product = []
+with open('product.csv', 'r', encoding='utf-8')as f:
+	for line in f:
+		if '品項,價錢' in line:
+			continue
+		name, price =line.strip().split(',')
+		product.append([name,price])
+print(product)
+
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
-	proudct.append([name, price])
-print(proudct)
+	price = int(price)
+	product.append([name, price])
+print(product)
 
-for p in proudct:
+for p in product:
 	print(p[0], '的價格是', p[1])
+
+with open('product.csv', 'w', encoding='utf-8') as f:
+	f.write('品項,價錢\n' )
+	for p in product:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
